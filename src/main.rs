9cd60ed76943ca92fd24348675175733e1bc111d
rexsum <rexsum@kalib0x.attlocal.net>
2024-03-02 05:18:35 -0600

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+/// Usage: use pwin::pw
+
+/// print!("Enter Password:");
+/// let passwd = pw::Readpw;
+
+/// returns a non mutable String
+
+pub mod pw {
+    extern crate termion;
+
+    use termion::input::TermRead;
+    use std::io;
+
+    fn strip_nl(s: &mut String) -> String {
+        if s.ends_with('\n') {
+            s.pop();
+            if s.ends_with('\r') {
+                s.pop();
+            }
+        }
+        let out: String = s.to_string();
+        out
+    }
+
+    pub fn Readpw() -> String {
+        let stdin = io::stdin();
+        let mut stdin = stdin;
+        let stdout = io::stdout();
+        let mut stdout = stdout;
+
+        let passwd = TermRead::read_passwd(&mut stdin, &mut stdout);
+
+        let Ok(Some(mut password)) = passwd else { todo!() };
+        let pw = strip_nl(&mut password);
+        pw
+    }
+}
