0deea026564750c85846a5f6c02ee8b56682257a
Rex Sum <rexsum420@gmail.com>
2024-03-02 08:39:41 -0600

    Final fixes

--- src/pw.rs
+++ src/pw.rs
@@ -1,27 +1,27 @@
-/// Usage: use crate::readpw
+/// Usage: use crate::pwin::readpw
 
 /// print!("Enter Password:");
 /// let passwd = readpw();
 
 /// returns a non mutable String
 
-extern crate termion;
-
-use termion::input::TermRead;
-use std::io;
-
-fn strip_nl(s: &mut String) -> String {
-    if s.ends_with('\n') {
-        s.pop();
-        if s.ends_with('\r') {
-            s.pop();
-        }
-    }
-    let out: String = s.to_string();
-    out
-}
-
 pub mod pw {
+    extern crate termion;
+
+    use termion::input::TermRead;
+    use std::io;
+
+    fn strip_nl(s: &mut String) -> String {
+        if s.ends_with('\n') {
+            s.pop();
+            if s.ends_with('\r') {
+                s.pop();
+            }
+        }
+        let out: String = s.to_string();
+        out
+    }
+
     pub fn readpw() -> String {
         let stdin = io::stdin();
         let mut stdin = stdin;
